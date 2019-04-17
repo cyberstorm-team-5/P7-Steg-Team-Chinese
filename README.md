e8bd2a6e2bea6ccda99ed8668d64bf793f0dd73e
Andrew Maurice <acm1264@yahoo.com>
2019-04-17 08:42:17 -0500

    initial files added
    setup python and readme files, and added writeup and bit/byte files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# P7-Steg-Team-Chinese
-Program 7: Steg for CSC 442.
+# Program 7: Steg
+* Authors: Team Chinese (Lane Arnold, Christopher Boquet, Christopher Bouton, Darrell Durousseaux, Clay Fonseca, Rebecca Grantham, Andrew Maurice)
+* Class: Cyber Security 2019 (CSC 442)
+## About
+This repository contains the code and other resources used for the seventh programming assignment, Steg. The Python code will implement both the bit and byte steg algorithms for storing and retrieving data. The sentiel value will be six bytes appended left to right to the hidden data. 
---
+++ steg.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2.7
+################################################################################
+# Authors: Team Chinese (Lane Arnold, Christopher Boquet,
+# 	   Christopher Bouton, Darrell Durousseaux, Clay Fonseca,
+#	   Rebecca Grantham, Andrew Maurice)
+# Class: CSC 442
+# Date: 5-3-2019
+# Github Repo: https://github.com/cyberstorm-team-5/P7-Steg-Team-Chinese
+# Description: Program 7: Steg
+#              The Python code will implement both the bit and byte steg
+#              algorithms for storing and retrieving data. The sentiel value
+#              will be six bytes appended left to right to the hidden data. 
+################################################################################
+
+import sys
+
+################################################################################
+
+#global vars here
+DEBUG = True
+
+
+
+################################################################################
+
+#functions here
+
+
+
+###############################MAIN#############################################
+
+
+
+
